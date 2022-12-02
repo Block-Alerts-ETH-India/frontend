6d9cece53a234d6dd81b72315d122936239c997e
Prince Kumar Singh <princekumar2000.pks@gmail.com>
2022-12-03 04:36:47 +0530

    [Prince] Fix 01

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import Home from './pages/Home';
 function App() {
   return (
     <div className="App">
-      <AuthenticatedPage/>  
-      {/* <Home/> */}
+      {/* <AuthenticatedPage/>   */}
+      <Home/>
     </div>
   );
 }
