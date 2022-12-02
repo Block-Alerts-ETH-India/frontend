9c6a45df0d4f39fe9a1cce30fc8a201e8179411f
Prajna <prajnapprabhu3@gmail.com>
2022-12-03 04:11:29 +0530

    home page

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,10 +3,10 @@ import '../styles/Button.scss'
 
 const Button = ({prefixLogo,text,suffixLogo, customClass}) => {
   return (
-    <div className={`${customClass} button p-2 rounded-lg`}>
+    <div className={`${customClass} flex items-center  button p-2 rounded-lg`}>
         {prefixLogo}
         <p className='text-md font-semibold'>{text}</p>
-        {suffixLogo}
+        <p className='ml-2'>{suffixLogo}</p>
     </div>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,21 @@
-import React from 'react'
-import Button from './Button';
-import { FaBell } from 'react-icons/fa';
-import '../styles/Header.scss'
+import React from "react";
+import Button from "./Button";
+import { FaBell } from "react-icons/fa";
+import "../styles/Header.scss";
 
 const Header = () => {
   return (
- 
-<header className="header-wrapper flex justify-between p-10 mx-auto ">
-<div className="logo flex items-center justify-between text-3xl font-semibold">
-    <FaBell className='mr-2 logo-icon'/>
-    block<span className='logo-alert'>Alert</span>
-</div>
+    <header className="header-wrapper flex justify-between p-10 px-20  mx-auto ">
+      <div className="logo flex items-center justify-between text-3xl font-semibold">
+        <FaBell className="mr-2 logo-icon" />
+        block<span className="logo-alert">Alert</span>
+      </div>
 
-   
+      <div className="items-center cursor-pointer font-medium">
+        <Button text="Connect Wallet" />
+      </div>
+    </header>
+  );
+};
 
-    <div className="items-center cursor-pointer font-medium">
-    <Button  text="Connect Wallet" />
-    </div>
-
-</header>
-  )
-}
-
-export default Header
+export default Header;
