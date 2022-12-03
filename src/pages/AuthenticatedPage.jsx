a3420aa640edbd1e3689787624023a131b051e75
Prajna <prajnapprabhu3@gmail.com>
2022-12-04 01:12:43 +0530

    create alert page

--- src/pages/AuthenticatedPage.jsx
+++ src/pages/AuthenticatedPage.jsx
@@ -3,6 +3,7 @@ import Header from "../components/Header";
 import "../styles/AuthenticatedPage.scss";
 import Button from "../components/Button";
 import "../styles/Button.scss";
+import { Link } from 'react-router-dom'
 // import "../styles/ModalPop.scss"
 import {Modal } from 'antd';
 
@@ -26,21 +27,25 @@ const AuthenticatedPage = () => {
       </Modal>
 
       <div className="text-center flex justify-around items-center mt-20  flex-wrap">
-        <a onClick={showModal} href="#">
+        {/* <a onClick={showModal} href="#"> */}
           <div className="auth-box ">
             <img
               src="https://upload.wikimedia.org/wikipedia/commons/4/47/PNG_transparency_demonstration_1.png"
               alt=""
               srcset=""
             />
-            <div className="btn-bottom">
-              <Button
+            
+             <Link to="/create-alert">
+             <div className="btn-bottom">
+             <Button
                 customClass={"absolute text-center btn-border"}
                 text={"Create Alert"}
               ></Button>
-            </div>
+               </div>
+             </Link>
+           
           </div>
-        </a>
+        {/* </a> */}
         <div className="auth-box ">
           <img
             src="https://upload.wikimedia.org/wikipedia/commons/4/47/PNG_transparency_demonstration_1.png"
---
+++ src/pages/CreateAlert.jsx
@@ -0,0 +1,66 @@
+import React from 'react'
+import { useState } from 'react'
+import Header from '../components/Header'
+import '../styles/Create-alert.scss'
+
+const CreateAlert = () => {
+
+    const [name, setName] = useState('');
+    const [address, setAddress] = useState('');
+    const [slackhook, setSlackhook] = useState('');
+
+function handleSubmit(event) {
+        event.preventDefault();
+        console.log('name:', name);
+        console.log('address:', address);
+        console.log('slackhook:', slackhook);
+      }
+
+  return (
+<div>
+    <Header/>
+
+<div className='flex justify-center create-alert-wrapper'>
+
+
+
+<div className='flex justify-center items-center'>
+
+    <form action="" onSubmit={handleSubmit} className='flex flex-col justify-around items-center rounded-md py-4 form-wrapper'>
+        
+        <p className='text-3xl font-semibold'>Create Alert</p>
+
+        <div>
+        <label className='text-md font-medium' >Alert name</label><br/>
+        <input type="text"  placeholder='Enter alert name' className='px-6 py-2 rounded-md mt-2'  value={name}
+          onChange={(e) => setName(e.target.value)}  />
+        </div>
+
+      
+        <div>
+        <label className='text-md font-medium' >Alert name</label><br/>
+        <input type="text" placeholder='Enter alert name' className='px-6 py-2 rounded-md mt-2'  value={address}
+          onChange={(e) => setAddress(e.target.value)} />
+        </div>
+
+        <div>
+        <label className='text-md font-medium' >Alert name</label><br/>
+        <input type="text" placeholder='Enter alert name' className='px-6 py-2 rounded-md mt-2'  value={slackhook}
+          onChange={(e) => setSlackhook(e.target.value)} />
+        </div>
+
+       <button type='submit' className='bg-black py-2 px-8 rounded-lg' >Submit</button>
+
+    </form>
+
+</div>
+
+
+</div>
+
+
+</div>
+  )
+}
+
+export default CreateAlert
