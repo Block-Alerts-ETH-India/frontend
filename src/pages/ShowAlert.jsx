975f282c3d977df62f1151e22f859acf989cc77a
Prajna <prajnapprabhu3@gmail.com>
2022-12-04 07:45:35 +0530

    fetch data for alerts

--- src/pages/ShowAlert.jsx
+++ src/pages/ShowAlert.jsx
@@ -1,44 +1,56 @@
 import React from "react";
 import Header from "../components/Header";
+import "../styles/ShowAlert.scss";
 
 import { useState, useEffect } from "react";
 
 const ShowAlert = () => {
-
   const [alerts, setAlerts] = useState([]);
-  fetch("http://127.0.0.1:8080/alert")
-    .then(response => {
-      return response.json()
+  fetch("http://172.105.42.117/alert")
+    .then((response) => {
+      return response.json();
     })
-    .then(data => {
+    .then((data) => {
       console.log(data.data);
-      setAlerts(data.data)
-    })
+      setAlerts(data.data);
+    });
+
+  // const data = [
+  //   {
+  //     alertName: "someName",
+  //     alertAddress: "smartContractAddress",
+  //     alertSlackWebhook: "https://hooks.slack.com",
+  //   },
+  // ];
 
   return (
     <div>
       <Header />
-      <div className="title mt-10  p-20  font-bold text-4xl">
-        <h1>Show Alert</h1>
-        <div
-          style={{ height: "90px", padding: "10px" }}
-          className="flex  mb-8 flex-col justify-enter border-2 text-base"
-        >
-          Alert Name
-          <div className="hash">
-            Alert Address : Lorem ipsum dolor sit amet consectetur adipisicing
-            elit. Officiis?
-          </div>
-        </div>
-        <div
-          style={{ height: "90px", padding: "10px" }}
-          className="flex  flex-col justify-enter border-2 text-base"
-        >
-          Alert Name
-          <div className="hash">
-            Alert Address : Lorem ipsum dolor sit amet consectetur adipisicing
-            elit. Officiis?
-          </div>
+
+      <div className="alerts-show-wrapper flex flex-col justify-between  w-8xl mx-auto">
+        <p className="text-5xl font-semibold">Alerts</p>
+
+        <div className="flex flex-col justify-between alerts-container-wrapper">
+          {alerts.map((user) => (
+            <div className="flex flex-col mt-4 justify-around items-left py-4 px-10 alerts-container rounded-lg">
+              <p className="text-lg font-semibold">
+                Alert name:
+                <span className="text-base font-medium"> {user.alertName}</span>
+              </p>
+              <p className="text-lg font-semibold">
+                Smart contract adress:
+                <span className="text-base font-medium">
+                  {user.alertAddress}
+                </span>
+              </p>
+              <p className="text-lg font-semibold">
+                Web hook:
+                <span className="text-base font-medium">
+                  {user.alertSlackWebhook}
+                </span>
+              </p>
+            </div>
+          ))}
         </div>
       </div>
     </div>
